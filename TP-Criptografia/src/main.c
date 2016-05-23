dea08f0c5864c0e271509e9370aa70891ec52a60
nicmora <nicmora>
2016-05-22 22:12:58 -0300

    Ejemplo de como cifrar y descifrar, ahora solo falta hacerlo con el body de la imagen

--- TP-Criptografia/src/main.c
+++ TP-Criptografia/src/main.c
@@ -17,15 +17,26 @@ int main() {
 	 * Comente en el archivo ecrypt-config.h la linea 258 a 268 porque tiraba error de compilacion.
 	 */
 
-	ECRYPT_ctx* ctx;
-	const u8* ciphertext;
-	u8* plaintext;
-	u32 msglen = info.imgsize;
-	//ECRYPT_init();
-	//ECRYPT_keysetup();
-	//ECRYPT_ivsetup();
-	//ECRYPT_encrypt_bytes();
-	//ECRYPT_encrypt_packet();
-	//ECRYPT_keystream_bytes()
+	//Como Cifrar
+	ECRYPT_ctx ctx;
+	u8 key[16] = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0x12,0x34,0x56,0x78,0x9a,0xbc,0xde,0xf0},
+		IV[12] = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0x12,0x34,0x56,0x78};
+	char* plaintext = "Hola Mundooo";
+	u32 msglen = strlen(plaintext);
+	char ciphertext[msglen];
+	ECRYPT_init();
+	ECRYPT_keysetup(&ctx,key,128,96);
+	ECRYPT_ivsetup(&ctx,IV);
+	ECRYPT_encrypt_bytes(&ctx,plaintext,ciphertext,12);
+
+	//Como Descifrar
+	ECRYPT_ctx ctx2;
+	u32 msglen2 = strlen(ciphertext);
+	char plaintext2[msglen2];
+	ECRYPT_init();
+	ECRYPT_keysetup(&ctx2,key,128,96);
+	ECRYPT_ivsetup(&ctx2,IV);
+	ECRYPT_decrypt_bytes(&ctx2,ciphertext,plaintext2,12);
+
 	return 0;
 }
