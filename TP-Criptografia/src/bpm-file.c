fac0a8c3e8f0e2e73ec2d30a8955c2bef6223958
nicmora <nicmora>
2016-05-26 23:05:35 -0300

    Terminando el programa, Con errores

--- TP-Criptografia/src/bpm-file.c
+++ TP-Criptografia/src/bpm-file.c
@@ -43,12 +43,13 @@ unsigned char *LoadBMP(char *filename, bmpInfoHeader *bInfoHeader) {
 }
 
 unsigned char *SaveBMP(char *filename, char *data, uint32_t msglen) {
+
 	FILE *f;
 	bmpFileHeader header; /* cabecera */
 	bmpInfoHeader bInfoHeader;
 	uint16_t type; /* 2 bytes identificativos */
 
-	f = fopen(filename, "r");
+	f = fopen(filename, "r+");
 	if (!f)
 		return NULL; /* Si no podemos leer, no hay imagen*/
 
@@ -65,28 +66,15 @@ unsigned char *SaveBMP(char *filename, char *data, uint32_t msglen) {
 	/* Leemos la cabecera de información completa */
 	fread(&bInfoHeader, sizeof(bmpInfoHeader), 1, f);
 
-
-	FILE *f2;
-	f2 = fopen("C:\\boca2.bmp", "w");
-	if (!f)
-		return NULL;
-
-	/* Leemos los datos de imagen, tantos bytes como imgsize */
-	fwrite(&type, sizeof(uint16_t), 1, f2);
-	fwrite(&header, sizeof(bmpFileHeader), 1, f2);
-	fwrite(&bInfoHeader, sizeof(bmpInfoHeader), 1, f2);
-	fwrite(data, sizeof(char), msglen, f2);
-
 	/* Nos situamos en el sitio donde empiezan los datos de imagen,
 	 nos lo indica el offset de la cabecera de fichero*/
-	//fseek(f, header.offset, SEEK_SET);
+	fseek(f, header.offset, SEEK_SET);
 
 	/* Leemos los datos de imagen, tantos bytes como imgsize */
-	//fwrite(data, sizeof(char), strlen(data), f);
+	fwrite(data, sizeof(char), msglen, f);
 
 	/* Cerramos */
 	fclose(f);
-	fclose(f2);
 
 	/* Devolvemos la imagen */
 	return 0;
--- TP-Criptografia/src/main.c
+++ TP-Criptografia/src/main.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
 #include "ecrypt-sync.h"
 #include "bpm-file.h"
 
+void iniciar_programa();
+void abrir_imagen();
+void elegir_opcion(int *);
+void encriptar_imagen(char *, bmpInfoHeader *);
+void desencriptar_imagen(char *, bmpInfoHeader *);
+
+char *dir = "C:\\boca.bmp";
+
 int main() {
-  
-	//Abrir archivo bmp
 
-	bmpInfoHeader info;
-	char *plaintext;
-	plaintext = LoadBMP("C:\\boca.bmp", &info);
-	DisplayInfo(&info);
+	int opcion;
+	bmpInfoHeader header;
+	char *body;
 
-	//Encriptar
-	/*
-	 * Comente en el archivo ecrypt-config.h la linea 258 a 268 porque tiraba error de compilacion.
-	 */
+	iniciar_programa();
+	abrir_imagen(body, &header);
+	elegir_opcion(&opcion);
 
-	//Como Cifrar
-	ECRYPT_ctx ctx;
-	u8 key[16] = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0x12,0x34,0x56,0x78,0x9a,0xbc,0xde,0xf0},
-		IV[12] = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0x12,0x34,0x56,0x78};
-	u32 msglen = info.imgsize;
-	char ciphertext[msglen];
-	ECRYPT_init();
-	ECRYPT_keysetup(&ctx,key,128,96);
-	ECRYPT_ivsetup(&ctx,IV);
-	ECRYPT_encrypt_bytes(&ctx,plaintext,ciphertext,msglen);
-	SaveBMP("C:\\boca.bmp", ciphertext, msglen);
+	if(opcion == 1) {
+		encriptar_imagen(body, &header);
+	} else if(opcion == 2) {
+		desencriptar_imagen(body, &header);
+	}
 
 	//Como Descifrar
 	/*ECRYPT_ctx ctx2;
@@ -38,5 +37,50 @@ int main() {
 	ECRYPT_ivsetup(&ctx2,IV);
 	ECRYPT_decrypt_bytes(&ctx2,ciphertext,plaintext2,12);*/
 
-	exit(0);
+	return 0;
+}
+
+void iniciar_programa() {
+	printf("CriptoApp - Grain128a\n\n");
+	printf("Elegir una opción:\n");
+	printf("1 - Encriptar archivo.bmp\n");
+	printf("2 - Desencriptar archivo.bmp\n");
+	return;
+}
+
+void abrir_imagen(char *body, bmpInfoHeader *header) {
+	body = LoadBMP(dir,header);
+	return;
+}
+
+void elegir_opcion(int *opcion) {
+
+	scanf("%u",opcion);
+	printf("\n");
+
+	while(*opcion != 1 && *opcion != 2) {
+		printf("Opción incorrecta. Elegir una opción entre [1;2]\n");
+		scanf("%u",opcion);
+		printf("\n");
+	}
+
+	return;
+}
+
+void encriptar_imagen(char *plaintext, bmpInfoHeader *header) {
+	ECRYPT_ctx ctx;
+	u8 key[16] = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0x12,0x34,0x56,0x78,0x9a,0xbc,0xde,0xf0},
+		IV[12] = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0x12,0x34,0x56,0x78};
+	u32 msglen = header->imgsize;
+	char ciphertext[msglen];
+	ECRYPT_init();
+	ECRYPT_keysetup(&ctx,key,128,96);
+	ECRYPT_ivsetup(&ctx,IV);
+	ECRYPT_encrypt_bytes(&ctx,plaintext,ciphertext,msglen);
+	SaveBMP(dir, ciphertext, msglen);
+}
+
+void desencriptar_imagen(char *ciphertext, bmpInfoHeader *header) {
+	//TODO
+	return;
 }
